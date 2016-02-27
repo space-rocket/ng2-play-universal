45f238bdbff3ded9eda0ba67f93be84eb4dbfb60
Michael Chavez <bigbassroller@gmail.com>
2016-02-26 22:40:45 -0800

    added typings folder

---
+++ typings/tsd.d.ts
@@ -0,0 +1,6 @@
+/// <reference path="source-map/source-map.d.ts" />
+/// <reference path="source-map-support/source-map-support.d.ts" />
+/// <reference path="es6-shim/es6-shim.d.ts" />
+/// <reference path="node/node.d.ts" />
+/// <reference path="serve-static/serve-static.d.ts" />
+/// <reference path="mime/mime.d.ts" />
